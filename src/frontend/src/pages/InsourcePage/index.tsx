67a0c16b20d8a53bb5c2acc9cd1337931690cb4e
Warren <wwwc@outlook.com>
2023-09-06 16:18:00 +0800

    fix insource page

--- src/frontend/src/pages/InsourcePage/index.tsx
+++ src/frontend/src/pages/InsourcePage/index.tsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from "react";
 import { TabsContext } from "../../contexts/tabsContext";
 
 import { useNavigate } from "react-router-dom";
+import IconComponent from "../../components/genericIconComponent";
+import Header from "../../components/headerComponent";
 
 export default function InsourcePage(): JSX.Element {
     const { flows, setTabId, downloadFlows, uploadFlows, addFlow } =
@@ -19,8 +21,11 @@ export default function InsourcePage(): JSX.Element {
     }, []);
 
     return (
+        <>
+        <Header />
         <div style={{ height: "100%" }}>
             <iframe src={`${window.location.protocol}//${window.location.hostname}:8200`} style={{ width: "100%", height: "100%" }} />
         </div>
+        </>
     );
 }
