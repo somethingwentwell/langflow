37a8e33420ae78ad7815e342dd692a2ad40a3513
Warren <wwwc@outlook.com>
2023-09-13 15:55:35 +0800

    update insource page to admin only

--- src/frontend/src/pages/InsourcePage/index.tsx
+++ src/frontend/src/pages/InsourcePage/index.tsx
@@ -4,10 +4,20 @@ import { TabsContext } from "../../contexts/tabsContext";
 import { useNavigate } from "react-router-dom";
 import IconComponent from "../../components/genericIconComponent";
 import Header from "../../components/headerComponent";
+import { AuthContext } from "../../contexts/authContext";
 
 export default function InsourcePage(): JSX.Element {
     const { flows, setTabId, downloadFlows, uploadFlows, addFlow } =
         useContext(TabsContext);
+
+    const {
+        isAdmin,
+        isAuthenticated,
+        logout,
+        getAuthentication,
+        userData,
+        autoLogin,
+    } = useContext(AuthContext);
 
     // set null id
     useEffect(() => {
@@ -22,10 +32,16 @@ export default function InsourcePage(): JSX.Element {
 
     return (
         <>
-        <Header />
-        <div style={{ height: "100%" }}>
-            <iframe src={`${window.location.protocol}//${window.location.hostname}:8200`} style={{ width: "100%", height: "100%" }} />
-        </div>
+            <Header />
+            {isAdmin ? (
+                <div style={{ height: "100%" }}>
+                    <iframe src={`${window.location.protocol}//${window.location.hostname}:8200`} style={{ width: "100%", height: "100%" }} />
+                </div>
+            ) : (
+                <div style={{ textAlign: "center", marginTop: "50px" }}>
+                    <h2>Only admins can access this page.</h2>
+                </div>
+            )}
         </>
     );
 }
